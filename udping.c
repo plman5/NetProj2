a6b1ba6a5a8f61fca4a0bea9481761d2bf3129f7
adamH989 <134100682+adamH989@users.noreply.github.com>
2023-10-29 17:17:37 -0400

    Update threads
    Implemented threads. Need to get threading to work.

--- udping.c
+++ udping.c
@@ -1,6 +1,3 @@
-// Adam Harvey and Parker Hundley 
-// Assignment 2
-
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
@@ -8,13 +5,126 @@
 #include <netdb.h>
 #include "Practical.h"
 #include <getopt.h>
+#include <unistd.h>
 #include <pthread.h>
 #include <signal.h>
 #include <time.h>
-#include <unistd.h>
+
+struct ThreadData {
+
+int ping_count;
+int received_pings;
+struct timespec start_time;
+long long min_rtt;
+long long max_rtt;
+long long sum_rtt;
+double ping_interval;
+int port_number;
+char* server_ip_add;
+
+};
+
+
+void *senderThreadFunction(void *data) {
+    struct ThreadData *threadData = (struct ThreadData *)data;
+    int ping_count = threadData->ping_count;
+    char *server_ip_add = threadData->server_ip_add;
+    int port_number = threadData->port_number;
+    double ping_interval = threadData->ping_interval;
+
+    struct addrinfo *servAddr;
+    if (getServerAddress(server_ip_add, port_number, &servAddr) != 0) {
+        fprintf(stderr, "Error getting server address\n");
+        return NULL;
+    }
+
+    int sock = socket(servAddr->ai_family, servAddr->ai_socktype, servAddr->ai_protocol);
+
+    if (sock < 0) {
+        DieWithSystemMessage("socket() failed");
+        return NULL;
+    }
+
+    char echoString[MAXSTRINGLENGTH];
+    size_t echoStringLen;
+
+    for (int i = 0; i < ping_count; i++) {
+        // Construct the ping message (you can customize this part)
+        snprintf(echoString, sizeof(echoString), "Ping %d", i + 1);
+        echoStringLen = strlen(echoString);
+
+        // Send the ping message
+        ssize_t numBytes = sendto(sock, echoString, echoStringLen, 0,
+                                  servAddr->ai_addr, servAddr->ai_addrlen);
+
+        if (numBytes < 0) {
+            DieWithSystemMessage("sendto() failed");
+        } else if (numBytes != echoStringLen) {
+            DieWithUserMessage("sendto() error", "sent unexpected number of bytes");
+        }
+
+        // Sleep for the specified ping interval
+        if (i < ping_count - 1) {
+            struct timespec sleepTime;
+            sleepTime.tv_sec = (time_t)ping_interval;
+            sleepTime.tv_nsec = (long)((ping_interval - (time_t)ping_interval) * 1e9);
+
+            nanosleep(&sleepTime, NULL);
+        }
+
+        threadData->received_pings++;
+    }
+
+    freeaddrinfo(servAddr);
+    close(sock);
+
+    return NULL;
+}
+
+void *receiverThreadFunction(void *data) {
+    struct ThreadData *threadData = (struct ThreadData *)data;
+    int ping_count = threadData->ping_count;
+    int port_number = threadData->port_number;
+
+    struct addrinfo *servAddr;
+    if (getServerAddress(NULL, port_number, &servAddr) != 0) {
+        fprintf(stderr, "Error getting server address\n");
+        return NULL;
+    }
+
+    int sock = socket(servAddr->ai_family, servAddr->ai_socktype, servAddr->ai_protocol);
+
+    if (sock < 0) {
+        DieWithSystemMessage("socket() failed");
+        return NULL;
+    }
+
+    char buffer[MAXSTRINGLENGTH + 1];
+    socklen_t fromAddrLen;
+
+    for (int i = 0; i < ping_count; i++) {
+        ssize_t numBytes = recvfrom(sock, buffer, MAXSTRINGLENGTH, 0,
+                                    servAddr->ai_addr, &fromAddrLen);
+
+        if (numBytes < 0) {
+            DieWithSystemMessage("recvfrom() failed");
+        } else {
+            buffer[numBytes] = '\0';
+            printf("Received: %s\n", buffer);
+
+            // Calculate RTT here (you can customize this part)
+        }
+    }
+
+    freeaddrinfo(servAddr);
+    close(sock);
+
+    return NULL;
+}
+
 
 int main(int argc, char *argv[]){
-  
+
     int ping_count = 0x7fffffff;
     double ping_interval = 1.0;
     int port_number = 33333;
@@ -28,35 +138,52 @@ int main(int argc, char *argv[]){
         switch (opt) {
             case 'c':
                 ping_count = atoi(optarg);
-                fprintf(stderr, "Count: %d\n", ping_count);
+		fprintf(stderr, "Count: %d\n", ping_count);
                 break;
             case 'i':
                 ping_interval = atof(optarg);
-                fprintf(stderr, "Interval: %lf\n", ping_interval);
+		fprintf(stderr, "Interval: %lf\n", ping_interval);
                 break;
             case 'p':
-                port_number = atoi(optarg);
-                fprintf(stderr, "Port: %d\n", port_number);
-                break;
+		port_number = atoi(optarg);
+		fprintf(stderr, "Port: %d\n", port_number);
+	        break;
             case 's':
                 data_size = atoi(optarg);
-                fprintf(stderr, "Size: %d\n", data_size);
+		fprintf(stderr, "Size: %d\n", data_size);
                 break;
             case 'n':
                 no_print = 1; // Set the flag to print summary stats only
                 break;
             case 'S':
                 server_mode = 1; // Set the flag to operate in server mode
-                break;
-            default:
+		break;
+	default:
+                fprintf(stderr, "Usage: %s [-c count] [-i interval] [-p port] [-s size] [-n] [-S]\n", argv[0]);
                 exit(EXIT_FAILURE);
         }
     }
 
+struct ThreadData threadData;
+pthread_t senderThread, receiverThread;
+
+threadData.ping_count = ping_count;
+threadData.ping_interval = ping_interval;
+threadData.port_number = port_number;
+threadData.server_ip_add = server_ip_add;
+
 if(server_mode){
 
-  char server[6];
-  snprintf(server, sizeof(server), "%d", port_number);
+        if (port_number == -1) {
+            fprintf(stderr, "Server mode requires specifying a port with -p.\n");
+            exit(EXIT_FAILURE);
+        }
+
+  
+  char service[6];
+  snprintf(service, sizeof(service), "%d", port_number);
+
+
 
   // Construct the server address structure
   struct addrinfo addrCriteria;                   // Criteria for address
@@ -67,7 +194,7 @@ if(server_mode){
   addrCriteria.ai_protocol = IPPROTO_UDP;         // Only UDP socket
 
   struct addrinfo *servAddr; // List of server addresses
-  int rtnVal = getaddrinfo(NULL, server, &addrCriteria, &servAddr);
+  int rtnVal = getaddrinfo(NULL, service, &addrCriteria, &servAddr);
   if (rtnVal != 0)
     DieWithUserMessage("getaddrinfo() failed", gai_strerror(rtnVal));
 
@@ -109,6 +236,9 @@ if(server_mode){
     else if (numBytesSent != numBytesRcvd)
       DieWithUserMessage("sendto()", "sent unexpected number of bytes");
   }
+
+  close(sock);
+  exit(0);
 }
 
 else{
@@ -116,12 +246,15 @@ else{
  if (optind < argc) {
         server_ip_add = argv[optind];
         fprintf(stderr, "Server IP Address: %s\n", server_ip_add);
-    } 
-    
-  char *echoString = argv[2]; // Second arg: word to echo
+    } else {
+        fprintf(stderr, "Server IP Address is required.\n");
+        exit(EXIT_FAILURE);
+    }
 
   char server[6];
   snprintf(server, sizeof(server), "%d", port_number);
+
+  char *echoString = argv[2]; // Second arg: word to echo
 
   size_t echoStringLen = strlen(echoString);
   if (echoStringLen > MAXSTRINGLENGTH) // Check input length
@@ -144,45 +277,18 @@ else{
   if (rtnVal != 0)
     DieWithUserMessage("getaddrinfo() failed", gai_strerror(rtnVal));
 
-  // Create a datagram/UDP socket
-  int sock = socket(servAddr->ai_family, servAddr->ai_socktype,
-      servAddr->ai_protocol); // Socket descriptor for client
-  if (sock < 0)
-    DieWithSystemMessage("socket() failed");
 
-  // Send the string to the server
-  ssize_t numBytes = sendto(sock, echoString, echoStringLen, 0,
-      servAddr->ai_addr, servAddr->ai_addrlen);
-  if (numBytes < 0)
-    DieWithSystemMessage("sendto() failed");
-  else if (numBytes != echoStringLen)
-    DieWithUserMessage("sendto() error", "sent unexpected number of bytes");
-
-  // Receive a response
-  struct sockaddr_storage fromAddr; // Source address of server
-  // Set length of from address structure (in-out parameter)
-  socklen_t fromAddrLen = sizeof(fromAddr);
-  char buffer[MAXSTRINGLENGTH + 1]; // I/O buffer
-  numBytes = recvfrom(sock, buffer, MAXSTRINGLENGTH, 0,
-      (struct sockaddr *) &fromAddr, &fromAddrLen);
-  if (numBytes < 0)
-    DieWithSystemMessage("recvfrom() failed");
-  else if (numBytes != echoStringLen)
-    DieWithUserMessage("recvfrom() error", "received unexpected number of bytes");
-
-  // Verify reception from expected source
-  if (!SockAddrsEqual(servAddr->ai_addr, (struct sockaddr *) &fromAddr))
-    DieWithUserMessage("recvfrom()", "received a packet from unknown source");
-
-  freeaddrinfo(servAddr);
-
-  buffer[echoStringLen] = '\0';     // Null-terminate received data
-  printf("Received: %s\n", buffer); // Print the echoed string
-
-  close(sock);
-  exit(0);
+       if (pthread_create(&senderThread, NULL, senderThreadFunction, &threadData) != 0) {
+            DieWithSystemMessage("pthread_create(senderThread) failed");
+        }
+        if (pthread_create(&receiverThread, NULL, receiverThreadFunction, &threadData) != 0) {
+            DieWithSystemMessage("pthread_create(receiverThread) failed");
+        }
 
 }
+
+    pthread_join(senderThread, NULL);
+    pthread_join(receiverThread, NULL);
 
     return 0;
 }
